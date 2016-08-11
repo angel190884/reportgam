d1437deb7f3ff84cb1c5dda320ed7ea05e806b8e
Angel Daniel Peregrino Juárez <angel190884@gmail.com>
2016-08-11 00:00:56 +0000

    test get and post

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return 'get';
+    }
+    public function store()
+    {
+        return 'post';
+    }
+}
